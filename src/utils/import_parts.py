8c9afc87f0c97527e250c55cfc70b343f1654197
Alex Baikov <baikov52@gmail.com>
2020-12-25 15:57:26 +0300

    change import_parts func

--- src/utils/import_parts.py
+++ src/utils/import_parts.py
@@ -1,5 +1,20 @@
-from .ftp import connect_to_ftp, get_csv_from_ftp, close_ftp
-from autoparts.models import Part
+from .ftp import connect_to_ftp, get_csv_from_ftp, close_ftp, get_data_from_csv
+from parts.models import Part
 
-def import_parts():
-    pass
+def import_parts(filename):
+    '''
+    Create parts from local csv file
+    '''
+    data = get_data_from_csv(filename)
+    for row in data:
+        print(row)
+        # _, created = Part.objects.get_or_create(
+        #     vin = row[0],
+        #     code = row[1],
+        #     name = row[2],
+        #     unit = row[3],
+        #     count = row[4],
+        #     )
+
+def __main__ (self):
+    import_parts()
