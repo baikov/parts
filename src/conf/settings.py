41239ae548ded8335e6d08fd7961fe0c3ab61689
Alex Baikov <baikov52@gmail.com>
2020-12-28 13:00:50 +0300

    fix settings

--- src/conf/settings.py
+++ src/conf/settings.py
@@ -53,7 +53,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
